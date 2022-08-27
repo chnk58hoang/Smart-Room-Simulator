85fd75293c9b4ec1eba92ef73c6cc93b5a47ec30
Trần Minh Hoàng (Artorias) <71833423+chnk58hoang@users.noreply.github.com>
2022-08-27 13:52:43 +0700

    cleancode

--- train.py
+++ train.py
@@ -124,5 +124,5 @@ if __name__ == '__main__':
 
     module = SpeechModule(model, train_dataloader, valid_dataloader, device)
     trainer = pl.Trainer(max_epochs=args.epoch, checkpoint_callback=checkpoint_callback(), callbacks=[call_back, ],
-                         accelerator='gpu', gpus=1)
+                         accelerator='gpu', gpus=0)
     trainer.fit(module)
